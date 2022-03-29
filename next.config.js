b62802e8e9a5bfc403e89bf18d83048bcd81c44b
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2022-03-28 20:18:02 -0700

    number one

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['avatarfiles.alphacoders.com']
+  }
 }
 
 module.exports = nextConfig
